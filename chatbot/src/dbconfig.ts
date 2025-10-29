3642ce52066b754cb7658deb8a45a1c685673b47
Madhubalan-R <142676147+Madhubalan-R@users.noreply.github.com>
2025-10-29 17:22:01 +0530

    Updated the ENV username

--- chatbot/src/dbconfig.ts
+++ chatbot/src/dbconfig.ts
@@ -6,7 +6,7 @@ export const AppDataSource = new DataSource({
 type: "mysql",
 host: process.env.DB_HOST,
 port: Number(process.env.DB_PORTNUMBER),
-username: process.env.DB_USER,
+username: process.env.DB_USERNAME,
 password: process.env.DB_PASSWORD,
 database: process.env.DB_NAME,
 synchronize: true,
